de4d53911f7a4debcdbde675fed29479eff567d9
Viktoriay Pekarskaya <pekarskayv@gmail.com>
2021-12-16 19:44:40 +0300

    Initial commit

---
+++ src/bsu/rfe/java/group10/lab5/Pekarskaya/varA/Main.java
@@ -0,0 +1,10 @@
+package bsu.rfe.java.group10.lab5.Pekarskaya.varA;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(3);
+        frame.setVisible(true);
+    }
+}
